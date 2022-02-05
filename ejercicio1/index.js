5faa20c9cda4c6cf86e7478f860be1daaad798c2
bladigit <ing.fernandez.bladimir@gmail.com>
2022-02-05 12:54:47 -0400

    ejercicio1

---
+++ ejercicio1/index.js
@@ -0,0 +1,12 @@
+// base 16 * 23
+// altura 13 cm
+const volumen = 16 * 23 * 13;
+console.log('volumen', volumen + ' cm3');
+
+// A = al + 2ab
+const arealateral = 23 * 13;
+const areaBase = 16 * 23;
+const areaTotal = arealateral + 2*areaBase;
+console.log('areaTotal', areaTotal + ' cm2')
+
+
